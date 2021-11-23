32b2cfe9bfdd04b38850e98d8b8c92e86afd9a10
Patricio Pagano <pagano.patricio@gmail.com>
2021-11-23 00:02:35 -0300

    home page: ok & not-found page: ok

--- src/app/pages/not-found/not-found.module.ts
+++ src/app/pages/not-found/not-found.module.ts
@@ -4,14 +4,10 @@ import { CommonModule } from '@angular/common';
 import { NotFoundRoutingModule } from './not-found-routing.module';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 
+import { MaterialModule } from 'src/app/material/material.module';
 
 @NgModule({
-  declarations: [
-    NotFoundComponent
-  ],
-  imports: [
-    CommonModule,
-    NotFoundRoutingModule
-  ]
+  declarations: [NotFoundComponent],
+  imports: [CommonModule, NotFoundRoutingModule, MaterialModule],
 })
-export class NotFoundModule { }
+export class NotFoundModule {}
